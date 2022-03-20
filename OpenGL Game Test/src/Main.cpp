d0143780d4feb088a6815e4b01c190eff60e3a2b
BJL156 <bjl15634@gmail.com>
2022-03-20 11:31:19 -0400

    Window class setup

--- OpenGL Game Test/src/Main.cpp
+++ OpenGL Game Test/src/Main.cpp
@@ -1,10 +1,12 @@
-#include <GLAD/glad.h>
-#include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+#include "headers/Window.h"
 
 int main()
 {
+	Window gameWindow = Window(860, 600, "OpenGL Game Test");
 
+	while (!gameWindow.WindowShouldClose())
+	{
+		gameWindow.Update(50.0f, 75.0f, 100.0f, 255.0f);
+	}
+	gameWindow.Destory();
 }
---
+++ OpenGL Game Test/src/headers/Window.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <GLAD/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+class Window
+{
+private:
+	int m_Width, m_Height;
+	const char* m_Title;
+	GLFWwindow* m_Window;
+public:
+	Window(int width, int height, const char* title) : m_Width(width), m_Height(height), m_Title(title)
+	{
+		glfwInit();
+
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+		glfwWindowHint(GLFW_RESIZABLE, false);
+
+		m_Window = glfwCreateWindow(m_Width, m_Height, m_Title, NULL, NULL);
+		if (m_Window == NULL)
+			std::cout << "FAILED TO CREATE WINDOW" << std::endl;
+		glfwMakeContextCurrent(m_Window);
+
+		if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+			std::cout << "GLAD FAILED TO INITIALIZE" << std::endl;
+	}
+
+	bool WindowShouldClose() { return glfwWindowShouldClose(m_Window); }
+	void SetWindowShouldClose(bool value) { return glfwSetWindowShouldClose(m_Window, value); }
+
+	int GetWidth() { return m_Width; }
+	int GetHeight() { return m_Height; }
+	GLFWwindow* GetWindow() { return m_Window; }
+
+	void Update(float r, float g, float b, float a)
+	{
+		glClearColor(r / 255.0f, g / 255.0f, b / 255.0f, a / 255.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		glfwSwapBuffers(m_Window);
+		glfwPollEvents();
+	}
+
+	void Destory()
+	{
+		glfwTerminate();
+	}
+};
